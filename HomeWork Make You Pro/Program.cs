54dc24db31a9b12089f4eaa42bb5c9d870889b54
Sergye_Naughty <shalby555@gmail.com>
2024-07-29 00:15:27 +0300

    Veriables

---
+++ HomeWork Make You Pro/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeWork_Make_You_Pro
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(Variables.CalculationOfCourierDelivery(100, 10, 10));	
+		}
+	}
+}
---
+++ HomeWork Make You Pro/Variables.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_Make_You_Pro
+{
+	public static class Variables
+	{
+	 public static int CalculationOfCourierDelivery (int a, int b, int c)
+		{
+			int result = a * c + b;
+			return result;
+		}
+	}
+}

704a12609cca1a19ef05d6792fa09668716951e0
Sergye_Naughty <shalby555@gmail.com>
2024-07-30 13:46:21 +0300

    Constructions

--- HomeWork Make You Pro/ConditionalConstructions.cs
+++ HomeWork Make You Pro/ConditionalConstructions.cs
@@ -11,33 +11,21 @@ namespace HomeWork_Make_You_Pro
 	{
 		public static int EmployeeBonus(int a, int b)
 		{
+			int result = 0;
 			if (a < b)
 			{
-				int result = a * 5 / 100;
+				 result = a * 5 / 100;
 			}
 			else if (a >= b)
 			{
-				int result = (a * 10 / 100);
+				 result = (a * 10 / 100);
             }
-			
+			return result;
 		}
 
-		public static int DiscountCard (int a, int b,int c)
+		public static bool IsDiscountCard (int a, int b,int c)
 		{
-			if (a >= 60)
-
-			{
-				Console.WriteLine("Скидка предоставляется");
-			}
-			else if (b >= 4 && c >= 5000)
-			{
-				Console.WriteLine("Скидка предоставляется");
-			}
-			else
-			{
-				Console.WriteLine("Скидка не предоставляется");
-			}
-			return
+		   return a >= 60 || b >= 4 && c >= 5000;
 		}
 	}
 }
